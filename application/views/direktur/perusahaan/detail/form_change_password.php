7031994181128d61b631de6aeb97baacb9fa9f26
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2022-05-23 20:04:04 +0700

    -

---
+++ application/views/direktur/perusahaan/detail/form_change_password.php
@@ -0,0 +1,18 @@
+<input type="hidden" name="user_unique_id" value="<?= $director_id ?>">
+<input type="hidden" name="user_role" value="<?= $user_role ?>">
+
+<div class="form-row mb-3">
+	<div class="col-12">
+   	<label for="user_password">Password baru <span class="text-danger small">*</span></label>
+   	<input type="password" name="user_password" id="user_password" class="form-control" placeholder="Ketik Password baru" autocomplete="off">
+   	<div class="invalid-feedback"></div>
+ 	</div>
+</div>
+
+<div class="form-row mb-3">
+	<div class="col-12">
+   	<label for="password_confirm">Konfirmasi Password <span class="text-danger small">*</span></label>
+   	<input type="password" name="password_confirm" id="password_confirm" class="form-control" placeholder="Ketik ulang password baru" autocomplete="off">
+   	<div class="invalid-feedback"></div>
+ 	</div>
+</div>
