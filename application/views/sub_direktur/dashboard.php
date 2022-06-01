07a95baa29cf25b7d5c925b896bccf527b802568
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2022-06-01 11:18:04 +0700

    Add Sub_direktur view

---
+++ application/views/sub_direktur/dashboard.php
@@ -0,0 +1,12 @@
+<!-- Content Starts -->
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="mb-1">Selamat Datang, <span class="text-primary"><?= user_login()->user_fullname ?></span></h4>
+                <span class="text-xs text-muted"><?= user_login()->user_email ?></span>
+                <p class="mb-0 text-sm text-secondary"></p>
+            </div>
+        </div>
+    </div>
+</div>
