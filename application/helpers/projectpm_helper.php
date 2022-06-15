eab9414bbbacd9e02a318a4b491b28c587684ee9
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2022-06-15 18:16:50 +0700

    add new helper for pm

---
+++ application/helpers/projectpm_helper.php
@@ -0,0 +1,16 @@
+<?php 
+
+function count_project_pm($projectStatus = 'on_progress') {
+	$company_id = user_company()->company_id;
+	$pm_id = user_company()->user_id;
+	$ci =& get_instance();
+	$ci->db->from('tb_project');
+	if ($projectStatus == 'on_progress') {
+		$ci->db->where(['ID_company' => $company_id, 'ID_pm' => $pm_id, 'project_status' => 'on_progress']);
+	} else if ($projectStatus == 'finish') {
+		$ci->db->where(['ID_company' => $company_id, 'ID_pm' => $pm_id, 'project_status' => 'finish']);
+	} else if ($projectStatus == 'archive') {
+		$ci->db->where(['ID_company' => $company_id, 'ID_pm' => $pm_id, 'project_archive' => 1]);
+	}
+	return $ci->db->count_all_results();
+}
